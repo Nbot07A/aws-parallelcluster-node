c9b80e1405a98f11e87d2f6fd593d54df023d282
Nse Obot <nseobot@amazon.com>
2022-09-01 15:12:10 -0400

    add slurm_node_is_drained tests

--- tests/common/schedulers/test_slurm_commands.py
+++ tests/common/schedulers/test_slurm_commands.py
@@ -509,18 +509,32 @@ def test_slurm_node_has_job(node, expected_output):
     "node, expected_output",
     [
         (SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "somestate", "queue1"), False),
-        (SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "MIXED+POWERING_UP+CLOUD+DRAIN", "queue1"), False),
+        (SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "MIXED+POWERING_UP+CLOUD+DRAIN", "queue1"), True),
         (
             SlurmNode(
                 "queue1-st-c5xlarge-1", "nodeip", "nodehostname", "ALLOCATED+Not_RESPONDING+CLOUD+DRAIN", "queue1"
             ),
-            False,
+            True,
         ),
         (
             SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "IDLE+Not_RESPONDING+CLOUD+DRAIN", "queue1"),
             True,
         ),
         (SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+DRAIN", "queue1"), True),
+        (
+                SlurmNode("queue1-dy-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+REBOOT_ISSUED", "queue1"),
+                False,
+        ),
+        (
+                SlurmNode(
+                    "queue1-st-c5xlarge-1", "nodeip", "nodehostname", "MIXED+CLOUD+DRAIN+REBOOT_REQUESTED", "queue1"
+                ),
+                True,
+        ),
+        (
+                SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+DRAIN+REBOOT_ISSUED", "queue1"),
+                True,
+        ),
     ],
 )
 def test_slurm_node_is_drained(node, expected_output):
